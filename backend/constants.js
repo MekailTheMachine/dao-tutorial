beb33294e37589d12787efbb203393c83016cfd6
Mekail The Machine <mekailmurphy@gmail.com>
2023-03-27 18:04:26 -0600

    Lw3DaO

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xBC56732772d3618b6056f4894FCFF07380dFADa1";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
